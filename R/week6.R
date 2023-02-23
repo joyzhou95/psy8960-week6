f94c7a12412056d4ef245e78aec840ef8f14698a
Joy Zhou <zhou1559@umn.edu>
2023-02-23 17:03:47 -0600

    Imported data & removed empty string lines

--- R/week6.R
+++ R/week6.R
@@ -1,7 +1,10 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(stringi)
+library(stringr)
 
 
 
-
-
+# Data Import
+citations <- stri_read_lines("../data/citations.txt", encoding = "Windows-1258")
+citation_txt <- str_subset(citations, pattern = "")
