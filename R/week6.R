a132b0fe61632a5b9bda4a0365e37bca588fc10e
Joy Zhou <zhou1559@umn.edu>
2023-02-23 17:12:55 -0600

    Sample 10 citations

--- R/week6.R
+++ R/week6.R
@@ -10,3 +10,6 @@ citations <- stri_read_lines("../data/citations.txt", encoding = "Windows-1258")
 citation_txt <- str_subset(citations, pattern = "")
 length(citations) - length(citation_txt)
 sum(str_length(citation_txt))/length(citation_txt)
+
+# Data Cleaning
+sample(citation_txt, 10)
