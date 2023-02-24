37fc229aea2bce594bf72eac51db692302e3f64e
Joy Zhou <zhou1559@umn.edu>
2023-02-23 18:36:15 -0600

    Edited the pattern for subsetting

--- R/week6.R
+++ R/week6.R
@@ -7,9 +7,10 @@ library(stringr)
 
 # Data Import
 citations <- stri_read_lines("../data/citations.txt", encoding = "Windows-1258")
-citation_txt <- str_subset(citations, pattern = "")
+citation_txt <- str_subset(citations, pattern = ".")
 length(citations) - length(citation_txt)
 sum(str_length(citation_txt))/length(citation_txt)
 
 # Data Cleaning
 sample(citation_txt, 10)
+
