54c9f22d002703e26031036de5af9b529a64188b
Joy Zhou <zhou1559@umn.edu>
2023-02-24 16:29:40 -0600

    Created citation tibble & corrected line 12

--- R/week6.R
+++ R/week6.R
@@ -2,15 +2,24 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
 library(stringr)
-
+library(tidyverse)
 
 
 # Data Import
 citations <- stri_read_lines("../data/citations.txt", encoding = "Windows-1258")
 citation_txt <- str_subset(citations, pattern = ".")
 length(citations) - length(citation_txt)
-sum(str_length(citation_txt))/length(citation_txt)
+mean(str_length(citation_txt))
 
 # Data Cleaning
 sample(citation_txt, 10)
+
+citation_tbl <- tibble(lines = 1:length(citation_txt), cite = citation_txt)
+
+  
+
+
+
+
+
 
