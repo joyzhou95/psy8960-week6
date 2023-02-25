04b9124d3e84507a3e8bec9dd43d379de40341e0
Joy Zhou <zhou1559@umn.edu>
2023-02-25 15:17:53 -0600

    Corrected the codes for creating the year and page_start variables

--- R/week6.R
+++ R/week6.R
@@ -7,7 +7,7 @@ library(rebus)
 
 # Data Import
 citations <- stri_read_lines("../data/citations.txt", encoding = "Windows-1258")
-citation_txt <- str_subset(citations, pattern = ".")
+citation_txt <- str_emp(citations, pattern = ".+")
 length(citations) - length(citation_txt)
 mean(str_length(citation_txt))
 
@@ -16,10 +16,7 @@ sample(citation_txt, 10)
 head(citation_txt, 20)
 citation_tbl <- tibble(lines = 1:length(citation_txt), cite = citation_txt) %>%
   mutate(cite = str_replace_all(string = cite, pattern = "\"|'", replacement = "")) %>%
-  mutate(year = str_extract(cite, '\\d{4}')) %>%
-  mutate(page_start = str_extract(cite, '\\s\\d+-')) %>%
-  mutate(perf_ref = str_detect(cite, pattern = '[Pp]erformance|PERFORMACE')) %>%
-  
-
-
+  mutate(year = str_match(cite, pattern = '\\((\\d+)\\)')[,2]) %>%
+  mutate(page_start = str_match(cite, pattern = '\\s(\\d+)-')[,2]) %>%
+  mutate(perf_ref = str_detect(cite, pattern = '[Pp]erformance|PERFORMANCE')) 
 
