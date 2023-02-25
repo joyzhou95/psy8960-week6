f3c7626caf18802de465437b4ecd8f51ea6d5120
Joy Zhou <zhou1559@umn.edu>
2023-02-25 15:56:42 -0600

    Simplied the codes for the perf_ref variable

--- R/week6.R
+++ R/week6.R
@@ -18,6 +18,7 @@ citation_tbl <- tibble(lines = 1:length(citation_txt), cite = citation_txt) %>%
   mutate(cite = str_replace_all(string = cite, pattern = "\"|'", replacement = "")) %>%
   mutate(year = str_match(cite, pattern = '\\((\\d+)\\)')[,2]) %>%
   mutate(page_start = str_match(cite, pattern = '\\s(\\d+)-')[,2]) %>%
-  mutate(perf_ref = str_detect(cite, pattern = '[Pp]erformance|PERFORMANCE')) %>%
-  mutate(title = str_match(cite, pattern = '\\(\\d+\\)\\.\\s([^\\.]+\\.)\\s')[,2])
+  mutate(perf_ref = str_detect(cite, pattern = fixed("performance", ignore_case = T))) %>%
+  mutate(title = str_match(cite, pattern = '\\(\\d+\\)\\.\\s([^\\.]+\\.)\\s')[,2]) %>%
+  
 
