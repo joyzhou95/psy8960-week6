162550e8d98d601c3cfb82b65d56a3a67ffdbfa3
Joy Zhou <zhou1559@umn.edu>
2023-02-25 16:36:16 -0600

    Createtd the variable first_author & cleaned up the codes

--- R/week6.R
+++ R/week6.R
@@ -13,12 +13,12 @@ mean(str_length(citation_txt))
 
 # Data Cleaning
 sample(citation_txt, 10)
-head(citation_txt, 20)
+
 citation_tbl <- tibble(lines = 1:length(citation_txt), cite = citation_txt) %>%
   mutate(cite = str_replace_all(string = cite, pattern = "\"|'", replacement = "")) %>%
   mutate(year = str_match(cite, pattern = '\\((\\d+)\\)')[,2]) %>%
   mutate(page_start = str_match(cite, pattern = '\\s(\\d+)-')[,2]) %>%
   mutate(perf_ref = str_detect(cite, pattern = fixed("performance", ignore_case = T))) %>%
   mutate(title = str_match(cite, pattern = '\\(\\d+\\)\\.\\s([^\\.]+\\.)\\s')[,2]) %>%
-  
+  mutate(first_author = str_match(cite, pattern = '^[A-Za-z-]+,\\s[A-Z]\\.\\s?[A-Z]?\\.?'))
 
