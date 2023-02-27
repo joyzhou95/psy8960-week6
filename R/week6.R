325844253227d7b3e302c5aa4d74a11d26decc1d
Joy Zhou <zhou1559@umn.edu>
2023-02-27 09:57:35 -0600

    Improved the codes of extacting title

--- R/week6.R
+++ R/week6.R
@@ -19,7 +19,7 @@ citation_tbl <- tibble(lines = 1:length(citation_txt), cite = citation_txt) %>%
   mutate(year = str_match(cite, pattern = '\\((\\d+)\\)')[,2]) %>%
   mutate(page_start = str_match(cite, pattern = '\\s(\\d+)-')[,2]) %>%
   mutate(perf_ref = str_detect(cite, pattern = fixed("performance", ignore_case = T))) %>%
-  mutate(title = str_match(cite, pattern = '\\(\\d+\\)\\.\\s([^\\.]+\\.)\\s')[,2]) %>%
+  mutate(title = str_match(cite, pattern = '\\(\\d+\\)\\.\\s([^\\.]+[\\(\\w\\)\\.]+)\\s')[,2]) %>%
   mutate(first_author = str_match(cite, pattern = '^[A-Za-z-]+,\\s[A-Z]\\.\\s?[A-Z]?\\.?'))
 
 sum(!is.na(citation_tbl$first_author))
